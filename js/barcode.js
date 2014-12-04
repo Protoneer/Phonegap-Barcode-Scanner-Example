a19dc3eb1a4d5d7c0435fbe3918fba697da732c4
protoneer <info@protoneer.co.nz>
2014-12-04 20:22:27 +1300

    made changes

--- js/barcode.js
+++ js/barcode.js
@@ -10,18 +10,3 @@ cordova.plugins.barcodeScanner.scan(
       alert("Scanning failed: " + error);
   });}
   
-window.plugins.flashlight.available(function(isAvailable) {
-  if (isAvailable) {
-
-    // switch on
-    window.plugins.flashlight.switchOn(); // success/error callbacks may be passed
-
-    // switch off after 3 seconds
-    setTimeout(function() {
-      window.plugins.flashlight.switchOff(); // success/error callbacks may be passed
-    }, 3000);
-
-  } else {
-    alert("Flashlight not available on this device");
-  }
-});
